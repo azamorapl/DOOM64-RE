30aacb02991e2a06a60049ea42501ea2fc08e1da
azamorapl <adolfo.zamorapl@gmail.com>
2023-06-28 14:29:58 -0500

    Allow saving before entering map32

--- doom64/src/g_game.c
+++ doom64/src/g_game.c
@@ -208,8 +208,16 @@ void G_RunGame (void) // 80004794
             return;
 
         /* run a stats intermission */
-        if (nextmap != 32) {
+        if (nextmap != 32)
+		{
             MiniLoop(IN_Start, IN_Stop, IN_Ticker, IN_Drawer);
+        } 
+		else if (EnableExpPak)
+		{
+			M_EncodePassword(Passwordbuff);
+        	CurPasswordSlot = 16;
+            MiniLoop(M_SavePakStart,M_SavePakStop,M_SavePakTicker,M_SavePakDrawer);
+			I_WIPE_FadeOutScreen();
         }
 
         if(((gamemap ==  8) && (nextmap ==  9)) ||
